c039f1fe4e3b832096d2522913782138ef88499c
TeukuNeo <teukuneoquinn@gmail.com>
2025-08-17 17:33:59 +0700

    Konten Pada Footer di Tambahkan

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,38 @@
 <footer>
- <p>Footer</p>
+    <div class="footer-container">
+        <div class="footer-section">
+            <ul>
+                <li class="footer-section-title">Tentang</li>
+                <li><a href="">Public Speaking</a></li>
+                <li><a href="">Literasi Kecerdasan Buatan</a></li>
+                <li><a href="">personal Branding</a></li>
+                <li><a href="">Freelancing Secara Mandiri</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li class="footer-section-title">Proyek</li>
+                <li><a href="">Proyek Klien</a></li>
+                <li><a href="">Proyek Otomasi & AI</a></li>
+                <li><a href="">Proyek Personal Branding</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li class="footer-section-title">Kontak</li>
+                <li><a href="">Lokasi Kami</a></li>
+                <li><a href="">Hubungi Kami</a></li>
+                <li><a href="">Alamat Email Kami</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li class="footer-section-title">Tentang</li>
+                <ul><a href="">Visi</a></ul>
+                <ul><a href="">Misi</a></ul>
+            </ul>
+        </div>
+    </div>
 </footer>
 <script src="{{ asset('js/index.js') }}"></script>
 </body>
