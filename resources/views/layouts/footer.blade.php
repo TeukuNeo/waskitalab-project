767397e93055b3fea38a2019aa9855ca47daf906
TeukuNeo <teukuneoquinn@gmail.com>
2025-08-18 00:29:46 +0700

    Konten Pada Footer di Tambahkan

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,19 @@
 <footer>
     <div class="footer-container">
-        <div class="footer-section">
-            <img src="{{ URL::to('/') }}/images/Waskita-Logo.png" width="100" height="50" alt="waskita logo">
-            <p>Kami Menawarkan Solusi </br>
+        <div class="footer-section" style="display: flex; flex-direction: column; gap: 15px;">
+            <img src="{{ URL::to('/') }}/images/Waskita-Logo.png" style="justify-content: start;" width="100" height="50" alt="waskita logo">
+            <p style="text-align: justify; font-weight: lighter; font-size: 10px;">Kami Menawarkan Solusi </br>
                 Mengenai Permasalahan </br>
                 Softskill Siswa</p>
+            <ul style="display: flex; flex-direction: row; gap:15px;">
+                <li><img src="{{ URL::to('/') }}/images/square-instagram-brands.png" width="20px" height="20px" alt="instagram"></li>
+                <li><img src="{{ URL::to('/') }}/images/square-facebook-brands.png" width="20px" height="20px" alt="facebook"></li>
+                <li><img src="{{ URL::to('/') }}/images/square-x-twitter-brands.png" width="20px" height="20px" alt="twitter"></li>
+                <li><img src="{{ URL::to('/') }}/images/square-linkedin-brands.png" width="20px" height="20px" alt="linkedin"></li>
+            </ul>
         </div>
         <div class="footer-section">
-            <ul>
+            <ul class="footer-caption">
                 <li class="footer-section-title">Tentang</li>
                 <li><a href="">Public Speaking</a></li>
                 <li><a href="">Literasi Kecerdasan Buatan</a></li>
@@ -16,7 +22,7 @@
             </ul>
         </div>
         <div class="footer-section">
-            <ul>
+            <ul class="footer-caption">
                 <li class="footer-section-title">Proyek</li>
                 <li><a href="">Proyek Klien</a></li>
                 <li><a href="">Proyek Otomasi & AI</a></li>
@@ -24,7 +30,7 @@
             </ul>
         </div>
         <div class="footer-section">
-            <ul>
+            <ul class="footer-caption">
                 <li class="footer-section-title">Kontak</li>
                 <li><a href="">Lokasi Kami</a></li>
                 <li><a href="">Hubungi Kami</a></li>
@@ -32,11 +38,24 @@
             </ul>
         </div>
         <div class="footer-section">
-            <ul>
+            <ul class="footer-caption">
                 <li class="footer-section-title">Tentang</li>
                 <ul><a href="">Visi</a></ul>
                 <ul><a href="">Misi</a></ul>
             </ul>
+        </div>
+    </div>
+    <hr style="border: 1px white solid;" width="100%" />
+    <div class="footer-credit">
+        <div>
+            <p>© 2025 WaskitaLab</p>
+        </div>
+        <div>
+            <p>Made with ❤️ from Pixabyte Bros</p>
+        </div>
+        <div style="display: flex; flex-direction: row; gap: 3px;">
+            <p>Syarat & Kondisi</p>
+            <p>Kebijakan Privasi</p>
         </div>
     </div>
 </footer>
